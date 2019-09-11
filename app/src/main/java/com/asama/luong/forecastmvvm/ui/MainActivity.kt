ad7f25714c95bbd20a9b134bd4ad0ff066b4ba3b
A Sama <channelasama@gmail.com>
2019-09-11 10:10:21 +0700

    Initial commit

---
+++ app/src/main/java/com/asama/luong/forecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.asama.luong.forecastmvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asama.luong.forecastmvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
